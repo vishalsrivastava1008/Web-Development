579ff51bb8593137a48e5db2850aedcfc08d043c
vishalsrivastava1008 <67066169+vishalsrivastava1008@users.noreply.github.com>
2021-07-19 14:05:58 +0530

    Add files via upload

---
+++ GRIP/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <style>
+      .bg{
+    width: 100%;
+    position:fixed;
+    z-index: -1;
+    opacity: 0.5;}
+      
+    </style>
+
+
+    </head>
+<body>
+<img class="bg" src="https://focus.cbbc.org/wp-content/uploads/2020/12/shutterstock_1150180799.jpg" alt="Bankingbg image">
+    
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">The Spark's Bank</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="viewcustomer.php">View Customers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="history.php">List of Transactions</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact us</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+<div class="container">
+    <div class="row">
+
+      <div class="col-sm-6">
+        <h1 class="main_text" ><i>The Spark's Bank</i></h1>
+        <p class="big-text">People's Service</p>
+      </div>
+
+      <div class="col-sm-6">
+        
+        <img src="https://www.bcsconsulting.com/wp-content/uploads/2018/12/bank-system-tile.png" class="img-fluid" width="560" height="345" style="margin-top:20px;">
+       
+      </div>
+
+   </div>
+   
+</div>
+
+</div>
+
+</div>
